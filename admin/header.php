efa0220e9219ef93b0ffff177149032386cead1d
abdelbassetaitbella <52855338+abdelbassetaitbella@users.noreply.github.com>
2020-12-07 00:23:45 +0100

    Add files via upload

---
+++ admin/header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+	<title>abdelbasset chattroom</title>
+	<script src="../js/jquery.min.js"></script>
+	<link rel="stylesheet" href="../css/bootstrap.min.css" />
+	<script src="../js/bootstrap.min.js"></script>
+	
+	<link href="../css/dataTables.bootstrap.css" rel="stylesheet">
+	<link href="../css/dataTables.responsive.css" rel="stylesheet">
+
+</head>
